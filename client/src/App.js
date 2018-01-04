26b60ab526fa4404648648fb92bff9c83b2c7bf8
Fausto Brito <fgbritom79@hotmail.com>
2018-01-04 01:59:01 -0500

    first commit under construction

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">HackStle</h1>
+        </header>
+        <iframe src="https://giphy.com/embed/HN8VCeLEUFj7a" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/please-construction-patient-HN8VCeLEUFj7a"></a></p>
+      </div>
+    );
+  }
+}
+
+export default App;
